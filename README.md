fa06c87ec864d318018581a6a961752ced038fca
pauloWeslleY <weslleyspfc@13outlook.com>
2023-02-22 23:16:55 -0300

    [FIX] README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# proj_weslley
-## My project developed with HTML5, SCSS, JavaScript
+# Meu Primeiro Portfolio em HTML SCSS JS
 
+![Imagem do Site](/src/img/layout.png)
+
+### Site desenvolvido com base em publicar e mostrar todos meus projetos pessoais e projetos freelancer
+
+### tecnologias usadas no desenvolvimento
+
+- HTML5
+- SCSS
+- JavaScript
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 "use strict";
-// ! NOTE: Menujs Scripts
+// ! NOTE: Menu MObile Scripts
 
 let openNav = document.querySelector("#threeline-icon");
 openNav.onclick = () => {
     const navbar = document.querySelector("#navigation");
     let iconClose = document.querySelector("#threeline-icon");
     let iconOpen = document.querySelector("#threeline-icon");
+
     switch (navbar.className === "navigation") {
         case navbar:
             navbar.className += " menujs";
